075bb48d90b4803806a98b78b60ad5f47739efd2
terrence <terrence.budnik@gmail.com>
2022-09-14 14:22:09 -0500

    homepage

--- Assets/java/script.js
+++ Assets/java/script.js
@@ -1,25 +1,3 @@
-var searchTopic = $('#searchTopic');
-
-
-
-var topicChoice = "fantasy";
-
-var baseURL = "http://gutendex.com/books/?topic="+topicChoice;
-console.log(baseURL);
-
-fetch(baseURL)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
-
-   searchTopic.on("click", function(){
-
-   });
-
-
 // changing the value of thext area
 $('#textarea1').val('New Text');
 M.textareaAutoResize($('#textarea1'));
