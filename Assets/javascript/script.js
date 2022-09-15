faf2243347ddcd410d77408bf8916f1d0fc2b8f7
terrence <terrence.budnik@gmail.com>
2022-09-15 14:36:13 -0500

    added button

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -4,17 +4,6 @@ var data;
 
 
 
-// changing the value of thext area
-// $('#textarea1').val('New Text');
-// M.textareaAutoResize($('#textarea1'));
-
-// // dropdown on homepage
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.dropdown-trigger');
-//     var instances = M.Dropdown.init(elems, options);
-//   });
-
-
 searchGenrebtn.on("click", function (e) {
     e.preventDefault();
     var genreChoice = userInput.val();
